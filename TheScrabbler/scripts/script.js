2b00d3c3598bf4487c3f18b0e703abbfc8fbe648
Greg Barkans <gregbarkans@gmail.com>
2015-03-23 17:24:08 -0400

    Update script.js

--- TheScrabbler/scripts/script.js
+++ TheScrabbler/scripts/script.js
@@ -1,59 +1,72 @@
-var GRID = 7;
-function Model(){
-	var grid = initGrid();
-	this.grid = grid;
+// GLOBAL VARIABLES ================================================
+var ROWS = 7;
+var COLUMNS = 7;
+//==================================================================
 
-	function initGrid() {
+//==================MODEL CLASS=====================================
+function Model(){
+	// Local Variables
+	var grid = initGrid(ROWS, COLUMNS); // calls with global vars
+
+	// Fields
+	this.grid = grid;  
+
+	// Public Methods  
+
+	//--Static Methods
+
+	//--Instance Methods               
+
+	// Private Methods
+	function initGrid(rows, columns) {
 		var grid = [];
-		for (var i = 0; i < GRID; i++) {
+		for (var i = 0; i < rows; i++) {
 			var row = [];
-			for (var j = 0; j < GRID; j++) {
+			for (var j = 0; j < columns; j++) {
 				row.push("");
 			}
 			grid.push(row);
 		}
 		return grid;
-
 	};
 
-	console.table(grid);
-}
+	//console.table(grid);  (TESTING)
+
+} // end of class
+//==================================================================
+
+//==================CONTROLLER CLASS================================
+
+
+function Controller(){
+	View();
+	Model();
+} // end of Controller class
+//==================================================================
+
+
+//==================VIEW CLASS======================================
 
 function View(){
-	var width =100;
-	var height =80;
+	var width =50;  // eventually will be globals
+	var height =50;  // designating width of each cell
 	var canvasID = document.getElementById('canvas');
 	var ctx = canvasID.getContext('2d');
 
-	// how to draw to the canvas
-	for(var i=0;i<GRID;i++){
-	ctx.strokeStyle = "blue";	 
-	ctx.fillStyle = "red";		
-	ctx.beginPath();	// have to have this begin path thing
-	
-	ctx.lineWidth = 2;
-	ctx.rect(100+ width*i,50+ width*i,width,width);
-	//ctx.lineTo(100+width*i,500);
-	ctx.closePath();
+	// Draws the rectangles
+	for(var rows = 0; rows < ROWS; rows++) {
+		for (var cols = 0; cols < COLUMNS; cols++) {
+			ctx.strokeStyle = "black";
+			ctx.fillStyle = "yellow";
+			ctx.beginPath();
+			ctx.lineWidth = 1;
+			ctx.rect(width*rows, height*cols, width, height);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke(); 
+		}
+	}
 
-	ctx.fill();
-	ctx.stroke(); 
-}
-	//
+} // end of View Class
 
-	/*
-	How to call a function (class) within a function
-	Note that the View is being called at the bottom
 
-	var model = new Model();
-	model.grid[3][4] = "test";
-	console.table(model.grid);
-
-	*/
-
-}
-
-function Controller(){
-	
-}
-View()
